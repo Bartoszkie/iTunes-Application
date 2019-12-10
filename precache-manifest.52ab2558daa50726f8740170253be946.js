fcd484567bb4194f0bae1b8a263a3495d8a50f50
Bartoszkie <bartosz.slysz23@gmail.com>
2019-12-10 14:08:06 +0100

    Updates

---
+++ precache-manifest.52ab2558daa50726f8740170253be946.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d313ec9d54a4389a127d55f9acae7a2",
+    "url": "/pgs-itunes/index.html"
+  },
+  {
+    "revision": "10c5c0306badeb71aab1",
+    "url": "/pgs-itunes/static/css/main.c4564370.chunk.css"
+  },
+  {
+    "revision": "f44766fdca4200bd2367",
+    "url": "/pgs-itunes/static/js/2.04ebc792.chunk.js"
+  },
+  {
+    "revision": "10c5c0306badeb71aab1",
+    "url": "/pgs-itunes/static/js/main.d3cfacca.chunk.js"
+  },
+  {
+    "revision": "ab9a837b950da039bc73",
+    "url": "/pgs-itunes/static/js/runtime~main.7250b933.js"
+  }
+]);
